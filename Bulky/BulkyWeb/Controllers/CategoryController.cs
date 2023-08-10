0a3133e6ed4ed11f2b1d8b2b70519ec8693bce61
Henrique Grosscklauss <henriquegrossdeveloper@gmail.com>
2023-08-10 20:49:45 -0300

    Implements Create Category endpoint

--- Bulky/BulkyWeb/Controllers/CategoryController.cs
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
@@ -21,6 +22,14 @@ namespace BulkyWeb.Controllers
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            _context.Categories.Add(obj);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
